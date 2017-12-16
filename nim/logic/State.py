78dc5a8ab9c97270d7d40032c47e40817dad9009
Melissa Rahm <merait00@hs-esslingen.de>
2017-12-16 10:48:23 +0100

    Add to_binary_reprasentation to State class

--- nim/logic/State.py
+++ nim/logic/State.py
@@ -5,17 +5,45 @@ class State:
 
     def __init__(self, rows : List):
         """
-        rows are a list of np.array 
+        rows are a list of np.array
         """
         self.Rows = rows
 
     @staticmethod
     def get_start_state(nr_of_pearls_per_row : List):
+        """
+        returns a state to start the game
+        """
         rows = []
         for nr_of_perls in nr_of_pearls_per_row:
             rows.append(np.ones(nr_of_perls))
 
         return State(rows)
+
+    def to_binary_reprasentation(self):
+        """
+        returns the binary state of the given state
+        """
+        nr_of_pearls_per_row = []
+        for row in self.Rows:
+            nr_of_pearls_per_row.append(np.sum(row))
+
+        max_nr_of_pearls = max(nr_of_pearls_per_row)
+        max_length_of_binary = len("{0:b}".format(max_nr_of_pearls))
+
+        binary_rows = []
+        for nr_of_pearls in nr_of_pearls_per_row:
+            binary = "{0:b}".format(nr_of_pearls)
+            binary_row = np.array([])
+            for i in range(0, max_length_of_binary - len(binary)):
+                binary_row = np.append(binary_row, 0)
+            for i in range(0, len(binary)):
+                binary_row = np.append(binary_row, int(binary[i]))
+            binary_rows.append(binary_row)
+
+        return State(binary_rows)
+
+
 
 
 
